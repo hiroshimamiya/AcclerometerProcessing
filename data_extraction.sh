91d37c2c71b539a8bb5830607ab4e6c34b364cf1
Yacine Lapointe <yacinelapoine@hotmail.com>
2024-06-20 10:28:25 -0400

    Added a legend with the column numbers and the UDI/data they represent, for clarity

--- data_extraction.sh
+++ data_extraction.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-## 1. Extract the columns we want
+#### Columns and the data they are representing:
+# Column(s) : UDI
+# 1 : eid
+# 27 : 31-0.0 (Sex)
+# 28 : 34-0.0 (Birth year)
+# 97 : 52-0.0 (Month of birth)
+# 941 : 1289-0.0 (Cooked vegetable intake) 
+# 949 : 1309-0.0 (Fresh Fruit Intake)
+# 957 : 1329-0.0 (Oily fish consumption)
+# 1045 : 1558-0.0 (Alcohol consumption (frequency))
+# 7008 : 6138-0.0 (Education)
+# 10795 : 20116-0.0 (Smoking status)
+# 12128 : 21000-0.0 (Education)
+# 12132 : 21001-0.0 (BMI) 
+# 19789 : 90001-0.0 (Raw Accelerometry Data)
+# 8425-8560 : 20002-X.XX; Instances: [0,3]; Arrays: [0,33] (Non-cancer illness code, self-reported) 
+# 19114-19160 : 41271-0.XX; Arrays: [0,46] (Diagnoses - ICD9)
+# 17991-18018 : 41203-0.XX; Arrays: [0,27] (Diagnoses - main ICD9)
+# 18229-18258 : 41205-0.XX; Arrays: [0,29] (Diagnoses - secondary ICD9)
+# 17621-1722 : 40001-0.XX; Arrays: [0,2] (Underlying (primary) cause of death)
+# 17623-17652 : 40002-0.XX; Instances: [0,1]; Arrays: [0,14] (Contributory (secondary) causes of death - ICD10)
+# 18855-19113 : 41270-0.XX; Arrays: [0,258] (Diagnoses - ICD10)
+# 17911-1799 : 41202-0.XX; Arrays: [0,79] (Diagnoses - main ICD10)
+# 18019-18228 : 41204-0.XXX; Arrays: [0,209] (Diagnoses - secondary ICD10)
+####
+
+
+
+## 1. Extract the columns we wanted as well as the ones extracted by OxWearables
 
 # Note: We need "csvkit"
 
@@ -12,16 +40,13 @@ cat /lustre03/project/6008063/neurohub/UKB/Tabular/current.csv | head -1000 | cs
 
 #*** Does not include Townsend index ***#
 
-
 # Extract the Townsend index column seperately because it is not in the "current.csv" file - first 1000 rows:
-
 cat /lustre03/project/6008063/neurohub/UKB/Tabular/RAP/ukb_field_22189.csv| head -1000 > /home/yalap95/current_df/townsend.csv
-
 #*** I then merged current_subset.csv and townsend.csv using python: "pd.merge(current_df, ukb_field_22189_df, on='eid')"
 
 
 
-## 2. Extract only the columns used  replica of OxWearables
+## 2. Extract only the columns used by OxWearables
 
 cat /lustre03/project/6008063/neurohub/UKB/Tabular/current.csv | head -1000 | csvcut -c 1,27,28,97,1045,7008,10795,12128,12132,103,786,479,7244,7245,7246,98,99,100,19802,19968,\
 19970,19972,19801,19799,19798 > /home/yalap95/current_df/current_subset_2.csv
