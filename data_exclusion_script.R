e07f14a137614429b00b523caa9e99904dc3d463
Yacine Lapointe <yacinelapoine@hotmail.com>
2024-06-27 15:10:38 -0400

    R Script to for data exclusion I re-created with help from the OxWearable rmd script and the data extraction tree

---
+++ data_exclusion_script.R
@@ -0,0 +1,56 @@
+
+# Load/Install packages
+#library(dplyr)
+#library(readr)
+
+# Import all data for participants with accelerometer data
+dat <- read.csv("/home/yalap95/current_df/final_df.csv")
+
+# Exclusions: these following commands will only keep the data we are not seeking to exclude
+## 1. Low quality accelerometer data
+### 1.1. Devices poorly calibrated
+dat <- dat[dat$quality_good_calibration == "Yes", ]
+
+### 1.2 Participants for whom \>1% of values were clipped (fell outside the 
+###    sensor's range) before or after calibration:
+dat <- dat[(dat$clips_before_cal < 0.01*dat$total_reads) & (dat$clips_after_cal < 0.01*dat$total_reads) , ]
+
+### 1.3 Insufficient wear time
+dat <- dat[dat$quality_good_wear_time == "Yes", ] # OxWearable note: Note that this has already been calculated in UKB, 
+# we don't need to manually calculate it: https://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=90015
+
+### 1.4 Unrealistically high acceleration values
+dat <- dat[dat$overall_activity < 100, ]
+
+
+## 2. Cardiovascular disease diagnosis in HES prior to accelerometer wear (2013)
+
+### 2.1 Participants that reported having a stroke < 2013
+#### 2.1.1 stroke < 2013-01-01
+dat <- subset(dat, stroke_date >= as.Date("2013-01-01") | is.na(stroke_date))
+#### 2.1.2 ischaemic stroke < 2013-01-01 
+dat <- subset(dat, ischaemic_stroke_date >= as.Date("2013-01-01") | is.na(ischaemic_stroke_date))
+#### 2.1.3 intracerebral haemorrhage < 2013-01-01 
+dat <- subset(dat, intracerebral_haemorrhage_date >= as.Date("2013-01-01") | is.na(intracerebral_haemorrhage_date))
+
+### 2.2 Participants that reported having a MI < 2013
+#### 2.2.1 MI < 2013-01-01
+dat <- subset(dat, myocardial_infarction_date >= as.Date("2013-01-01") | is.na(myocardial_infarction_date))
+#### 2.2.2 STEMI < 2013-01-01
+dat <- subset(dat, STEMI_date >= as.Date("2013-01-01") | is.na(STEMI_date))
+#### 2.2.3 NSTEMI < 2013-01-01
+dat <- subset(dat, NSTEMI_date >= as.Date("2013-01-01") | is.na(NSTEMI_date))
+
+
+## 3. Missing data for ethnicity, education*, smoking status, alcohol consumption, or
+##    Townsend Deprivation Index
+keep_cols <- c('smoking_raw', 'alcohol_raw', 'ethnicity_raw', 'tdi_raw')
+dat <- dat[complete.cases(dat[, keep_cols]), ]
+
+###### *Note: Education is currently missing
+
+
+
+
+
+
